3e9f93684f9366f4e09e81991ccf579861572c9f
callsmasterofficia <partnerchc@outlook.com>
2023-06-05 15:04:06 +0530

    fasd

--- pages/404.js
+++ pages/404.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import Navbar from "../components/Navbar"
+import Navbar from '../components/Navbar'
 import Error from '../components/404'
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   return {
-    props:{
-      meta:{
-        title:"Disney Plus Watch Party - Watch Disney Plus with Your Friends Online",
-        description:"Disney Plus Watch Party allows you to watch Disney Plus with friends from different locations online and chat with each other while watching.",
-        keywords:"Disney Plus Watch Party, Watch Party Disney Plus, How to do a Watch Party on Disney Plus, Disney Plus Party, Disney Watch Party, Disney Party, Watch Party on Disney Plus, How to Host a Disney Plus Watch Party, Disney Plus Group Watch",
-        pageUrl:"https://disneypluswatchparty.com/",
-        featuredImage:"/logo.png"
-      }
-    }
+    props: {
+      meta: {
+        title:
+          'Disney Plus Watch Party - Watch Disney Plus with Your Friends Online',
+        description:
+          'Disney Plus Watch Party allows you to watch Disney Plus with friends from different locations online and chat with each other while watching.',
+        keywords:
+          'Disney Plus Watch Party, Watch Party Disney Plus, How to do a Watch Party on Disney Plus, Disney Plus Party, Disney Watch Party, Disney Party, Watch Party on Disney Plus, How to Host a Disney Plus Watch Party, Disney Plus Group Watch',
+        pageUrl: 'https://disneypluswatchparty.com/',
+        featuredImage: '/logo.png',
+      },
+    },
   }
 }
 function error() {
   return (
-    <div className='error_404 '>
+    <div className="error_404 ">
       <div>
-        <Navbar/>
+        <Navbar />
       </div>
-      <Error/>
+      <Error />
     </div>
   )
 }
