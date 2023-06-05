42af4f979d4f85969b552f65c676faf025372130
yash <yashvermacallsmaster@gmail.com>
2023-06-05 14:54:47 +0530

    afasd

--- components/CustomAccordion.jsx
+++ components/CustomAccordion.jsx
@@ -8,7 +8,7 @@ export default function CustomAccordion({ item, show, toggleFunc }) {
       <div>
         <div
           onClick={() => toggleFunc(item)}
-          className="my-2 flex cursor-pointer items-center  justify-between rounded-md border-b-2 border-white bg-[#303030] p-4 font-semibold text-white"
+          className="my-2 flex cursor-pointer items-center  justify-between rounded-md border-b-2 border-white bg-[#1e3a8a] p-4 font-semibold text-white"
         >
           {item.question}
           <div className="flex  h-[24px] w-[24px] items-center  justify-center rounded-full bg-white font-bold text-black">
