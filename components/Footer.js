45e4059cd5fe5ff24dbc1ed86ab488c38ca26d45
NikhilRajput <nikhilcallsmaster12@gmail.com>
2024-03-22 16:07:09 +0530

    pldsj

--- components/Footer.js
+++ components/Footer.js
@@ -2,63 +2,100 @@ import React from 'react'
 import Link from 'next/link'
 import InstallButton from './InstallButton'
 import siteConfig from '../siteConfig'
+import { useRouter } from 'next/router'
+import { languageMap } from '../constants/languages'
+import { i18n } from '../next.config'
+function Footer({ installBtn, footer }) {
+  const router = useRouter()
+  const localeChangeHandler = (e) => {
+    const locale = e.target.value
 
-function Footer() {
+    router.push(
+      {
+        route: router.pathname,
+        query: router.query,
+      },
+      router.asPath,
+      { locale }
+    )
+  }
   return (
     <div className="footer-container mt-[20%] w-screen md:mt-[10%]">
       <div className="footer-box -mt-14 border-t-2 border-white"></div>
 
       <div className=" mt-8 flex items-center  justify-center">
         <p className="primary_text_color pb-5 text-xl font-semibold">
-          {siteConfig.footer.subheading}
+          {footer.letUsKnow}
         </p>
       </div>
       <div className="heading flex items-center justify-center">
         <h3 className="primary_text_color text-xl font-bold md:text-3xl">
-          {siteConfig.footer.title}
+          {footer.anyQuestions}
         </h3>
       </div>
 
-      <div className="btn-box m-10 flex w-auto items-center justify-center border-b-2 border-white">
+      <div className="btn-box m-10 flex w-auto items-center justify-center gap-2 border-b-2 border-white">
         <div className="install_btn -mt-6 mr-10 mb-5 items-center justify-center md:mt-0">
-          <InstallButton />
+          <InstallButton installBtn={installBtn} />
         </div>
 
         <button className="mb-5 -mt-6 flex w-auto items-center justify-center rounded-[4px] border-2 border-blue-100 bg-white p-2 text-xl transition-all duration-200 hover:bg-blue-100/90 md:mt-0">
           <Link href={`${siteConfig.blogUrl}`}>
             <span className="px-2 text-base font-bold text-black   transition-all duration-300">
-              Blog
+              {footer.blog}
             </span>
           </Link>
         </button>
-
-        <Link href="/privacy">
-          <div className="privacyBtnBox mb-5 -mt-6 ml-4 flex items-center justify-center rounded-[4px] bg-white p-2 text-xl  transition-all duration-200 hover:bg-blue-100/90 md:mt-0">
-            <button className="px-2 text-base font-bold text-black transition-all  duration-100 hover:text-white">
-              Privacy
-            </button>
-          </div>
-        </Link>
+        <button className="mb-5 -mt-6 flex w-auto items-center justify-center rounded-[4px] border-2 border-blue-100 bg-white p-2 text-xl transition-all duration-200 hover:bg-blue-100/90 md:mt-0">
+          <Link href={`${siteConfig.privacyUrl}`}>
+            <span className="px-2 text-base font-bold text-black   transition-all duration-300">
+              {footer.privacy}
+            </span>
+          </Link>
+        </button>
+        <div className="-mt-6 mb-5 flex w-auto items-center justify-center rounded-[4px]  p-2 text-xl transition-all duration-200  md:mt-0">
+          <select
+            onChange={localeChangeHandler}
+            className="mt-2 h-8 w-[100px] rounded-md pl-3"
+            value={router.locale}
+          >
+            {i18n.locales.map((item) => (
+              <option value={item} key={item}>
+                {languageMap[item]}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className="popu_search_container mb-4">
         <div className="popu_search_box  -mt-6 flex items-center justify-center p-2 ">
           <span className="span  flex items-center">
             <img
               className="h-8 w-8 object-contain pr-2"
+              width={8}
+              height={8}
               src="https://img.icons8.com/ios-glyphs/344/ffffff/right3.png"
               alt="side_icon"
             />
             <h4 className="mr-1 text-sm font-bold text-white md:text-lg">
-              Popular Searches
+              {footer.popularSearches} :
             </h4>
-            <span className="mr-1 text-white">:</span>
+            <span className="mr-2 text-white">:</span>
             <p className="text-xs text-white md:text-base">
-              {siteConfig.footer.tags.map((item, index) => {
-                return (
-                  <span className="pr-2" key={`tags-${index}`}>
-                    {item},
-                  </span>
-                )
+              {footer.footerTags.map((item, index) => {
+                if (index == 0 || index == 1) {
+                  return (
+                    <span className="pr-2" key={`tags-${index}`}>
+                      {item},
+                    </span>
+                  )
+                } else {
+                  return (
+                    <span className="pr-2" key={`tags-${index}`}>
+                      {item} ,
+                    </span>
+                  )
+                }
               })}
             </p>
           </span>
@@ -66,11 +103,11 @@ function Footer() {
 
         <div className="popu_search_box  flex items-center justify-center p-2">
           <span className="span  flex items-center">
-            <h4 className="mr-1 text-base font-bold text-white">Disclaimer</h4>
+            <h4 className="mr-1 text-base font-bold text-white">
+              {footer.disclaimer}
+            </h4>
             <span className="mr-1 text-white">:</span>
-            <p className="text-base text-white">
-              {siteConfig.footer.disclaimer}
-            </p>
+            <p className="text-base text-white">{footer.disclaimerContent}</p>
           </span>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,19 @@ import Text2 from '../components/Text2'
 import Fask_ques from '../components/Fask_ques'
 import Footer from '../components/Footer'
 import Script from 'next/script'
+import { getAllmeta, getHome } from '../locale/index'
 
-export async function getStaticProps() {
+export async function getStaticProps({ locale }) {
+  const home = getHome(locale)
+  const allmeta = getAllmeta(locale)
   return {
     props: {
+      home,
+      allmeta,
       meta: {
-        title:
-          'Bring back & relive Disney days with brats via Disney Watch Party ',
-        description:
-          'Now, you have Disney Watch Party that enables watching all your favorite Disney videos online from anywhere in real-time sync with live chat to bring amplification.     ',
-        keywords:
-          'Disney Plus Watch Party, Watch Party Disney Plus, How to do a Watch Party on Disney Plus, Disney Plus Party, Disney Watch Party, Disney Party, Watch Party on Disney Plus, How to Host a Disney Plus Watch Party, Disney Plus Group Watch',
+        title: allmeta.homeMetaTitle,
+        description: allmeta.homeMetadescription,
+        keywords: allmeta.homeMetakeywords,
         pageUrl: 'https://disneypluswatchparty.com/',
         featuredImage: '/logo.png',
       },
@@ -23,17 +25,16 @@ export async function getStaticProps() {
   }
 }
 
-const Home = () => {
+const Home = ({ home }) => {
   return (
     <div>
       <Script src="https://cdn.tailwindcss.com" />
-
-      <Navbar />
-      <Banner />
-      <Text />
-      <Text2 />
-      <Fask_ques />
-      <Footer />
+      <Navbar installBtn={home.header.installBtn} />
+      <Banner heading={home.header.navBarHeading} />
+      <Text installBtn={home.header.installBtn} home={home} />
+      <Text2 installBtn={home.header.installBtn} home={home} />
+      <Fask_ques home={home} />
+      <Footer installBtn={home.header.installBtn} footer={home.footer} />
 
       <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></Script>
     </div>
